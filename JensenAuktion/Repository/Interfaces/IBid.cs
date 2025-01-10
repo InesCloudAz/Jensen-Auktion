0c30603945a26d4a5458f1ecdef001a72b0aa520
Sravani Kandula <sravz.kandula@gmail.com>
2025-01-10 10:58:09 +0100

    add bid entity class

---
+++ JensenAuktion/Repository/Interfaces/IBid.cs
@@ -0,0 +1,11 @@
+﻿using JensenAuktion.Repository.Entities;
+
+namespace JensenAuktion.Repository.Interfaces
+{
+    public interface IBid
+    {
+        Bid CreateBid(Bid bid);
+        Bid DeleteBid(Bid bid);
+
+    }
+}
