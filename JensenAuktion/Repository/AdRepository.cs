e40f86e7bc07f7764ff93da04f525857643a9f45
gurdip bola <gurdip6@live.se>
2025-01-15 10:36:53 +0100

    working on add

---
+++ JensenAuktion/Repository/AdRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace JensenAuktion.Repository
+{
+    public class AdRepository
+    {
+        private readonly AuctionDbContext _context;
+
+        public AdRepository(AuctionDbContext context)
+        {
+            _context = context;
+        }
+    }
+
+}
