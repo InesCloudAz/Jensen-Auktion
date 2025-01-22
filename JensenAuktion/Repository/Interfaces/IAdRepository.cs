6c51eaa5da5c347e0ead96359b936bc031f07b1e
gurdip bola <gurdip6@live.se>
2025-01-22 14:30:30 +0100

    updatebids

--- JensenAuktion/Repository/Interfaces/IAdRepository.cs
+++ JensenAuktion/Repository/Interfaces/IAdRepository.cs
@@ -5,9 +5,17 @@ namespace JensenAuktion.Repository.Interfaces
 {
     public interface IAdRepository
     {
-        public void CreateAd(Ad ad);
-        public void DeleteAd(int id);
-        public List<Ad> GetAllAds();
-        public void UpdateAd(Ad ad);
+        void CreateAd(Ad ad);
+        void DeleteAd(int id);
+        Ad GetAdById(int adID);
+        List<Ad> GetAllAds();
+        void UpdateAd(Ad ad); // Added method definition
+
+        bool DeleteAdIfNoBids(int adId);
+        bool UpdateAdWithBidCheck(Ad ad);
     }
+            
+        
+    
 }
+   
--- JensenAuktion/Repository/Repos/BidRepo.cs
+++ JensenAuktion/Repository/Repos/BidRepo.cs
@@ -9,48 +9,61 @@ namespace JensenAuktion.Repository.Repos
 {
     public class BidRepo : IBidRepo
     {
-        
-            private readonly IJensenAuctionContext _context;
+        private readonly IJensenAuctionContext _context;
 
-            public BidRepo(IJensenAuctionContext context)
+        public BidRepo(IJensenAuctionContext context)
+        {
+            _context = context;
+        }
+
+        public int CreateBid(Bid bid)
+        {
+            using (var db = _context.GetConnection())
             {
-               _context = context;
-            }
+                db.Open();
 
-            public int CreateBid(Bid bid)
-            {
-                using (var db = _context.GetConnection())
-                {
-                    db.Open();
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@Price", bid.Price);
+                parameters.Add("@AdID", bid.AdID);
+                parameters.Add("@UserID", bid.UserID);
+                parameters.Add("@NewBidID", dbType: DbType.Int32, direction: ParameterDirection.Output);
 
-                    DynamicParameters parameters = new DynamicParameters();
-                    parameters.Add("@Price", bid.Price);
-                    parameters.Add("@AdID", bid.AdID);
-                    parameters.Add("@UserID", bid.UserID);
-                    parameters.Add("@NewBidID", dbType: DbType.Int32, direction: ParameterDirection.Output);
+                db.Execute("CreateBid", parameters, commandType: CommandType.StoredProcedure);
 
-                    db.Execute("CreateBid", parameters, commandType: CommandType.StoredProcedure);
-
-                    
-                    return parameters.Get<int>("@NewBidID");
-                }
-            }
-
-            public bool DeleteBid(int bidID)
-            {
-                using (var db = _context.GetConnection())
-                {
-                    db.Open();
-
-                    DynamicParameters parameters = new DynamicParameters();
-                    parameters.Add("@BidID", bidID);
-                    parameters.Add("@IsDeleted", dbType: DbType.Boolean, direction: ParameterDirection.Output);
-
-                    db.Execute("DeleteBid", parameters, commandType: CommandType.StoredProcedure);
-
-                   
-                    return parameters.Get<bool>("@IsDeleted");
-                }
+                return parameters.Get<int>("@NewBidID");
             }
         }
+
+        public bool DeleteBid(int bidID)
+        {
+            using (var db = _context.GetConnection())
+            {
+                db.Open();
+
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@BidID", bidID);
+                parameters.Add("@IsDeleted", dbType: DbType.Boolean, direction: ParameterDirection.Output);
+
+                db.Execute("DeleteBid", parameters, commandType: CommandType.StoredProcedure);
+
+                return parameters.Get<bool>("@IsDeleted");
+            }
+        }
+
+        public bool HasBids(int adId)
+        {
+            using (var db = _context.GetConnection())
+            {
+                db.Open();
+
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@AdID", adId);
+                parameters.Add("@HasBids", dbType: DbType.Boolean, direction: ParameterDirection.Output);
+
+                db.Execute("HasBids", parameters, commandType: CommandType.StoredProcedure);
+
+                return parameters.Get<bool>("@HasBids");
+            }
+        }
+    }
 }
